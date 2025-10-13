3e868a8018b3244b9e38de11843bae879d0d6098
PythonImporter <271374667@qq.com>
2025-10-13 10:14:36 +0800

    ♻️ refactor: 将QThreadPoolExecutor类分离到独立文件
    将QThreadPoolExecutor类从qthread_with_return.py中分离到新的qthread_pool_executor.py文件，并更新__init__.py中的导入语句。这种模块化改进提高了代码的可维护性和可测试性，使每个类都有明确的职责。

--- qthreadwithreturn/__init__.py
+++ qthreadwithreturn/__init__.py
@@ -1,5 +1,6 @@
 from qthreadwithreturn import qthread_with_return
-from qthreadwithreturn.qthread_with_return import QThreadPoolExecutor, QThreadWithReturn
+from qthreadwithreturn.qthread_with_return import QThreadWithReturn
+from qthreadwithreturn.qthread_pool_executor import QThreadPoolExecutor
 
 __all__ = [
     "QThreadWithReturn",
